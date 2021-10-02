409d7df1f4f9ca7ffa04cef74fb19df248280e95
Geoffrey Vander Cruyssen <geoffrey.vandercruyssen@gmail.com>
2021-10-02 19:24:16 +0200

    Blueprint and structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# La Pou Pou music bot
+Personal discord music bot to enjoy some tunes while chatting
+
+## Commands
+- `!join` - join your channel
+- `!play` - play a song
+- `!pause` - pause the current song
+- `!stop` - stop playing audio
+- `!disconnect` - leave the discord server
+- `!skip` - skip current song
+- `!ping` - check latency
+- `!hoya` - ask for a hoya
---
+++ app.py
@@ -0,0 +1,91 @@
+import logging
+
+import youtube_dl
+from discord.ext import commands
+from pydantic import BaseSettings
+from pydantic.types import SecretStr
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv(verbose=True)
+except Exception:
+    logging.error("Failed to load env vars")
+
+
+class BotConfig(BaseSettings):
+    BOT_TOKEN: SecretStr
+
+
+config = BotConfig()
+
+YTDL_OPTIONS = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+BOT_STATUS = [
+    "Offline",
+    "Jamming to some tunes",
+    "Idle",
+]
+
+ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
+musicbot = commands.Bot(command_prefix="!")
+
+
+# EVENTS
+@musicbot.event
+async def on_ready():
+    pass
+
+
+@musicbot.event
+async def on_disconnect():
+    pass
+
+
+# COMMANDS
+@musicbot.command(name="play", help="Plays audio, or adds it to the queue if something is playing")
+async def play(ctx):
+    pass
+
+
+@musicbot.command(name="pause", help="Pauses the current audio")
+async def pause(ctx):
+    pass
+
+
+@musicbot.command(name="join", help="Disconnect the bot from the server")
+async def join(ctx):
+    pass
+
+
+@musicbot.command(name="disconnect", help="Disconnect the bot from the server")
+async def disconnect(ctx):
+    pass
+
+
+@musicbot.command(name="skip", help="Skip The current audio")
+async def skip(ctx):
+    pass
+
+
+@musicbot.command(name="ping", help="Check latency of the bot")
+async def ping(ctx):
+    pass
+
+
+@musicbot.command(name="hoya", help="Ask for a hoooooooyaaaaaa")
+async def hoya(ctx):
+    # TODO pause playback and give a random hoya from a collection of hoyas?
+    pass
